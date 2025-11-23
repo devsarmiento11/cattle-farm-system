65e793b7f1e9b6bbd7dd6220e1818eb797c5cc5f
devsarmiento11 <dev.sarmiento11@gmail.com>
2025-11-24 01:37:16 +0800

    added new file

--- resources/views/home/aboutus.blade.php
+++
@@ -1,6 +0,0 @@
-<section class="about-us">
-        <strong>About Us</strong>
-        <p>
-            We provide safe and reliable livestock delivery directly to your location. All animals are transported using well-maintained, animal-friendly trailers to ensure their comfort and well-being during transit.
-        </p>
-    </section>
